fe30cf7cfc2d457fb3bfc1d4570bf746e8776828
psj <silbaram79@naver.com>
2023-09-09 13:57:22 +0900

    단위 테스트 설정 추가 kotest 사용

--- src/test/kotlin/com/github/silbaram/elasticsearch/dynamic_query_dsl/expansion/BoolQueryTest.kt
+++ src/test/kotlin/com/github/silbaram/elasticsearch/dynamic_query_dsl/expansion/BoolQueryTest.kt
@@ -1,25 +1,27 @@
 package com.github.silbaram.elasticsearch.dynamic_query_dsl.expansion
 
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery
 import co.elastic.clients.elasticsearch._types.query_dsl.Query
 import co.elastic.clients.elasticsearch._types.query_dsl.TermQuery
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 
 class BoolQueryTest: FunSpec({
     test("bool query 생성") {
         val boolQuery = Query.Builder().boolQuery {
-//            filterQuery (
-//                Query.Builder().term(TermQuery.Builder().field("field1").value("value1").build()).build(),
-//                Query.Builder().term(TermQuery.Builder().field("field4").value("value4").build()).build()
-//            )
-
-            filterQuery {
-                listOf(Query.Builder().term(TermQuery.Builder().field("field1").value("value1").build()))
-            }
+            filterQuery (
+                Query.Builder().term(TermQuery.Builder().field("field1").value("value1").build()).build()
+            )
         }
-        println(boolQuery.build().toString())
-//        val query = boolQuery.build()
-//        val tempQuery = "Query: {\"bool\":{\"filter\":[]}}"
-//
-//        query.toString() shouldBe tempQuery
+
+        val boolQueryBuild = boolQuery.build()
+        val filterQueryBuild = (boolQueryBuild._get() as BoolQuery).filter()
+        val termQueryBuild = filterQueryBuild[0]
+
+        boolQueryBuild._kind() shouldBe Query.Kind.Bool
+        filterQueryBuild.size shouldBe 1
+        termQueryBuild.isTerm shouldBe true
+        termQueryBuild.term().field() shouldBe "field1"
+        termQueryBuild.term().value().stringValue() shouldBe "value1"
     }
 })
