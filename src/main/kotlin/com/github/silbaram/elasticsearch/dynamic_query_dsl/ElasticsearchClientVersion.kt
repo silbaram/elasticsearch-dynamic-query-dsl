d83d02fbf2ff233b8d48dbaa15376e9892953351
park sangjin <silbaram79@naver.com>
2025-08-18 22:38:16 +0900

    Read Elasticsearch client version from pom.properties

--- src/main/kotlin/com/github/silbaram/elasticsearch/dynamic_query_dsl/ElasticsearchClientVersion.kt
+++ src/main/kotlin/com/github/silbaram/elasticsearch/dynamic_query_dsl/ElasticsearchClientVersion.kt
@@ -1,7 +1,11 @@
 package com.github.silbaram.elasticsearch.dynamic_query_dsl
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient
+import java.util.Properties
 
-fun elasticsearchClientVersion(): String? {
-    return ElasticsearchClient::class.java.`package`?.implementationVersion
-}
+fun elasticsearchClientVersion(): String? =
+    ElasticsearchClient::class.java.classLoader
+        ?.getResourceAsStream("META-INF/maven/co.elastic.clients/elasticsearch-java/pom.properties")
+        ?.use { input ->
+            Properties().apply { load(input) }.getProperty("version")
+        }
