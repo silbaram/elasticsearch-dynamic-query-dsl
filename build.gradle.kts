f84f0b39c2658c2b0d9d4861472631a2ded74c6a
park sangjin <silbaram79@naver.com>
2025-08-21 22:11:55 +0900

    build: add elastic repository and jvm target

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,14 @@ plugins {
     kotlin("jvm") version "1.9.0"
 }
 
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 group = "com.github.silbaram"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://artifacts.elastic.co/maven")
 }
 
 val elasticsearchJavaVersion: String by project
@@ -15,6 +18,12 @@ dependencies {
     implementation("co.elastic.clients:elasticsearch-java:$elasticsearchJavaVersion")
     testImplementation("io.kotest:kotest-runner-junit5:5.7.1")
     testImplementation("io.kotest:kotest-assertions-core:5.7.1")
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
 
 tasks.withType<Test>().configureEach {
